6c50d9dd63970b28e116d9477456f80344585a52
shaurya <shauryatiwari120@gmail.com>
2025-02-28 19:53:29 +0530

    Group Anagrams

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -5,23 +5,19 @@ class Solution {
 
         Map<String, List<String>> map = new HashMap<String, List<String>>();
         // finding key of aal teh string .
-        for (String string : strs) {
-            int[] count = new int[26];
-            // making string to character array .
-            for (char c : string.toCharArray()) {
-                count[c - 'a']++; // count stat from a .
+        for (String inputstr : strs) {
+            // convert string to the characters array
+            char[] charaarray = inputstr.toCharArray();
+            // sort the characters array
+            Arrays.sort(charaarray);
+            // construct string of the characters value
+            String soretedstring = String.valueOf(charaarray);
+            if (!map.containsKey(soretedstring)) {// of not available
+                map.put(soretedstring, new ArrayList<>()); // empty list
+
             }
-            // now again create all the characters to string .
-            StringBuilder sb = new StringBuilder();
-            for (int i : count) {
-                sb.append("#");
-                sb.append(i);
-            }
-            String key = sb.toString();
-            if (!map.containsKey(key)) {
-                map.put(key, new ArrayList<>());
-            }
-            map.get(key).add(string);
+            // add string to the list .with the original itrated string .
+            map.get(soretedstring).add(inputstr);
         }
         return new ArrayList<>(map.values());
     }
